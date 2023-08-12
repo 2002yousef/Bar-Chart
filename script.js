b73134d7cc353323a4d2981e5c8259da9d2fab22
Yousef <yousef.wael2002@gmail.com>
2023-08-12 19:27:35 +0300

    toolpit remaining

--- script.js
+++ script.js
@@ -1,45 +1,103 @@
 const width = 800;
 const height = 400;
-
-var data = [10, 15, 20, 25, 30];
-
-/*var x = '';
+const barWidth = width / 275
 
 fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
 .then(response => response.json())
-.then(data => {x = data.id; console.log(x)});
+.then(data => {
+    const arr = data.data
 
-console.log(lol)
-*/
+    const GPD = arr.map((item) => {
+        return item[1];
+    })
+    
+    var yearsDate = arr.map((item) => {
+        return new Date(item[0])
+    })
 
-let y = '';
+    var xMax = new Date(d3.max(yearsDate))
+    xMax.setMonth(xMax.getMonth() + 3)
 
-async function fetchData() {
-    const response = await fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json');
-    const data = await response.json();
-    const x = data.id;
-    y = x;
-    console.log(x);
-}
-
-fetchData()
-
-console.log(y)
-
-
-
-var svg = d3.select('.container')
+    var svg = d3.select('.container')
             .append('svg')
-            .attr('width', width)
-            .attr('height', height)
+            .attr('width', width + 100)
+            .attr('height', height + 60)
 
-var scale = d3.scaleLinear()
-                .domain([d3.min(data), d3.max(data)])
-                .range([0, width - 100])
+    var scaleX = d3.scaleTime()
+                    .domain([d3.min(yearsDate), xMax])
+                    .range([0, width])
 
-var x_axis = d3.axisBottom()
-                .scale(scale)
+    var x_axis = d3.axisBottom()
+                    .scale(scaleX)
 
-svg.append('g')
-    .call(x_axis)
+    svg.append('g')
+        .call(x_axis)
+        .attr('transform', 'translate(60, 400)')
+        .attr('id', 'x-axis')
+        
+
+    var scaleY = d3.scaleLinear()
+                    .domain([0, d3.max(GPD)])
+                    .range([height, 0])
+
+    var y_axis = d3.axisLeft()
+                    .scale(scaleY)
+
+    var scaledGPD = []
+    var linearScale = d3.scaleLinear()
+                        .domain([0, d3.max(GPD)])
+                        .range([0, height])
+
+    scaledGPD = GPD.map((item) => {
+        return(linearScale(item))
+    })
+    
+    svg.append('g')
+        .call(y_axis)
+        .attr('id', 'y-axis')
+        .attr('transform', 'translate(60, 0)')
+
+    svg.append('text')
+        .attr('transform', 'rotate(-90)')
+        .attr('x', -200)
+        .attr('y', 80)
+        .text('Gross Domestic Product');
+    
+    d3.select('svg')
+        .selectAll('rect')
+        .data(scaledGPD)
+        .enter()
+        .append("rect")
+        .style("fill", "steelblue")
+        .attr("class","bar")
+        .attr('x', (d,i) => {
+            return scaleX(yearsDate[i])
+        })
+        .attr('y', (d) => {
+            return height - d
+        })
+        .attr('width' , barWidth)
+        .attr('height', (d) => {
+            return d
+        })
+        .attr('transform', 'translate(60, 0)')
+        .attr('data-date', (d,i) => {
+            return arr[i][0]
+        })
+        .attr('data-gdp', (d,i) => {
+            return GPD[i];
+        })
+        .on('mouseover' , (event ,d) => {
+            d3.select(event.currentTarget)
+            .style("fill", "white");
+        })
+        .on('mouseout', (event, d) => {
+            d3.select(event.currentTarget)
+            .style("fill",'steelblue');
+        })
+        
+});
+
+
+
     
